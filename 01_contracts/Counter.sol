ce77e6d99fda1e6d6c82cbe51ce1aee68fe6404d
sangah323 <choa323@naver.com>
2025-04-28 11:44:37 +0900

    Fix : Counter.sol 수정

--- 01_contracts/Counter.sol
+++ 01_contracts/Counter.sol
@@ -13,14 +13,6 @@ contract Counter{
         count += 1;
     }
 
-    function decrement() public{
-        count -= 1;
-    }
-
-    function reset() public{
-        count = 0;
-    }
-
     function getCount() public view returns (uint256) {
         return count;
     }
