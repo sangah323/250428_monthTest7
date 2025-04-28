4a727499c211bb10834798432981bf4aad1e6db3
sangah323 <choa323@naver.com>
2025-04-28 15:57:04 +0900

    feat : 03_sepolia_tx/deploy_to_sepolia.js 작성

--- 01_contracts/Counter.sol
+++ 01_contracts/Counter.sol
@@ -13,6 +13,14 @@ contract Counter{
         count += 1;
     }
 
+    function decrement() public{
+        count -= 1;
+    }
+
+    function reset() public{
+        count = 0;
+    }
+
     function getCount() public view returns (uint256) {
         return count;
     }
